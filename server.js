501aea4e1b04a729e7652af0f6d7b192224f5f54
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2021-02-24 16:19:08 -0500

    update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+const dotenv = require('dotenv').config()
 
 const bodyParser = require('body-parser');
 // require handlebars
