8e470a6b0ecc825aca3f611087fec1d20076c8c2
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2021-03-02 14:34:04 -0500

    rework

--- db/migrations/20210228205643-add-map-id-to-completes.js
+++ db/migrations/20210228205643-add-map-id-to-completes.js
@@ -1,23 +1,23 @@
-'use strict';
+// 'use strict';
 
-module.exports = {
-  up: (queryInterface, Sequelize) => {
+// module.exports = {
+//   up: (queryInterface, Sequelize) => {
 
-    return queryInterface.addColumn('Completes', 'MapId', Sequelize.INTEGER).then(() => {
-      return queryInterface.addConstraint('Completes', ['MapId'], {
-        type: 'foreign key',
-        name: 'map_completes',
-        references: { //Required field
-          table: 'Map',
-          field: 'id'
-        },
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE'
-      });
-    });
-  },
+//     return queryInterface.addColumn('Completes', 'MapId', Sequelize.INTEGER).then(() => {
+//       return queryInterface.addConstraint('Completes', ['MapId'], {
+//         type: 'foreign key',
+//         name: 'map_completes',
+//         references: { //Required field
+//           table: 'Map',
+//           field: 'id'
+//         },
+//         onDelete: 'CASCADE',
+//         onUpdate: 'CASCADE'
+//       });
+//     });
+//   },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('Completes', 'MapId');  
-  }
-};
+//   down: (queryInterface, Sequelize) => {
+//     return queryInterface.removeColumn('Completes', 'MapId');  
+//   }
+// };
