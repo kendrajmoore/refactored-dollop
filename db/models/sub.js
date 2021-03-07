e29556256cafc2fd9b840d024e023c75c1aa16b4
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2021-03-07 14:39:15 -0500

    update

--- db/models/sub.js
+++ db/models/sub.js
@@ -3,7 +3,10 @@ module.exports = (sequelize, DataTypes) => {
   const Sub = sequelize.define('Sub', {
     username: DataTypes.STRING,
     email: DataTypes.STRING
-  }, {});
+  }, {
+    freezeTableName: true,
+    tableName: 'Subs',
+  });
   Sub.associate = function(models) {
     // associations can be defined here
     Sub.belongsTo(models.Map);
